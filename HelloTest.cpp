fbac506a7620854400ffa8542575338646fbd05f
yujin <jin5214151@gmail.com>
2022-03-18 14:41:20 +0900

    Add MulFunction

---
+++ HelloTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int mulFunction(int i, int j)
+{
+	return i * j;
+}
+
+void main
+{
+	cout << "Hello Git!" << endl;
+	cout << "mulFunction GOGO" << endl;
+	mulFunction(3, 5);
+}
